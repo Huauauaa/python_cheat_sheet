171ac4138fd9f3fd3a92a9278bb9e0b8b8838590
huaweiwei <harvey0379@163.com>
2022-01-04 16:25:27 +0800

    feat: list

--- high/index.py
+++ high/index.py
@@ -1,13 +1,45 @@
-prices = {
-    'AAPL': 191.88,
-    'GOOG': 1186.96,
-    'IBM': 149.24,
-    'ORCL': 48.44,
-    'ACN': 166.89,
-    'FB': 208.09,
-    'SYMC': 21.29,
-}
+# items = [0] * 3
+# print(items)
 
-p1 = {key: value for key, value in prices.items() if value > 100}
 
-print(p1)
+list1 = [1, 2, 3, 4]
+
+print(list1[3:2])
+
+# list2 = ['a', 'b']
+# list3 = ['A', 'B']
+
+# for (index, item) in enumerate(list2):
+#     print(index, item)
+
+# for x, y, z in zip(list1, list2, list3):
+#     print(f'{x}-{y}-{z}')
+
+
+# import json
+
+# with open('input.json', 'r', encoding='utf-8') as f:
+#     result = json.load(f)
+
+# print(json.dumps(result, indent=2, ensure_ascii=False))
+
+# print(json.dumps())
+
+# from collections import Counter
+
+# counter = Counter('Hello world!')
+# print(counter)
+
+# prices = {
+#     'AAPL': 191.88,
+#     'GOOG': 1186.96,
+#     'IBM': 149.24,
+#     'ORCL': 48.44,
+#     'ACN': 166.89,
+#     'FB': 208.09,
+#     'SYMC': 21.29,
+# }
+
+# p1 = {key: value for key, value in prices.items() if value > 100}
+
+# print(p1)
